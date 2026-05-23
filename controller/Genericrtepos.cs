2f27fcc7439a61c05c0469baf8f3a58a5d6da8e1
AhemdKhaled54 <ahmed.khsled0132@gmail.com>
2026-05-23 18:03:27 +0300

    added new Property

--- controller/Genericrtepos.cs
+++ controller/Genericrtepos.cs
@@ -2,8 +2,10 @@
 
 public class Class1
 {
-	public Class1()
-	{
-		public int UserId { get;set; }
-	}
+    public Class1()
+    {
+    public int UserId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
 }
