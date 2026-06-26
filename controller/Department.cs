4f279d2099e1e66405fff3d272a0fe4c0409ea12
AhemdKhaled54 <ahmed.khsled0132@gmail.com>
2026-06-26 19:09:51 +0300

    Update in Department class

--- controller/Department.cs
+++ controller/Department.cs
@@ -1,7 +1,20 @@
 ﻿using System;
 
-public class Dapartment 
+public class Dapartment
 {
-	public int Id { get; set; }
-	public string Name {  get; set; }
+    public int Id { get; set; }
+    public string F_Name { get; private set; }
+    public string L_Name { get; private set; }
+
+
+    public void SetName(string fname, string lname)
+    {
+        F_Name = fname;
+        L_Name = lname;
+    }
+
+
 }
+
+
+
